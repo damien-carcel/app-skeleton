c5429efb1cb55a6b4a313d2b6963fb0da3975970
Damien Carcel <damien.carcel@gmail.com>
2017-07-15 17:44:33 +0200

    Create an ES6 app skeleton with webpack

---
+++ README.md
@@ -0,0 +1,28 @@
+# ES 6 and webpack
+
+This is a basic application skeleton to easily bootstrap ES 6 applications with webpack.
+
+## How to use it
+
+You need to have `node` 6+ and `npm` 5+ installed on your computer.
+
+First install the dependencies:
+```bash
+$ npm install
+```
+
+Then run the test server:
+```bash
+$ npm run serve
+```
+
+Build the files for production environments by running:
+```bash
+$ npm run build
+```
+
+## License
+
+This repository is under the MIT license. See the complete license in the `LICENSE` file.
+
+The "Hack" font provided as asset example is distributed under [the Hack Open Font License v2.0 and the Bitstream Vera License](https://github.com/chrissimpkins/Hack/blob/master/LICENSE.md).
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const babelPresets = ['es2015', 'es2016', 'es2017'];
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  entry: {
+    app: './src/app.js',
+    print: './src/print.js'
+  },
+  output: {
+    filename: '[name].[chunkhash].min.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: babelPresets,
+            cacheDirectory: 'cache'
+          }
+        }
+      },
+      {
+        test: /\.(css|less)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader']
+        })
+      },
+      {
+        test: /\.(gif|jpg|png|svg)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(eot|otf|ttf|woff|woff2)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true
+      },
+      comments: false
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new ExtractTextPlugin('[chunkhash].min.css'),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: require('html-webpack-template'),
+      lang: 'en',
+      meta: [
+        {
+          name: 'description',
+          content: 'A better default template for html-webpack-plugin.'
+        }
+      ],
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      mobile: true,
+      title: 'My ES6 application skeleton'
+    })
+  ]
+};
