e17deda13687aa817ea50d11d12b3b7c66bddc7a
Damien Carcel <damien.carcel@akeneo.com>
2017-12-24 16:20:30 +0100

    Issue 13: Add a modal for post creation Use "react-modal" component

--- front/components/Create.jsx
+++ front/components/Create.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import ReactModal from 'react-modal';
 
 export default class Create extends React.Component {
-  render() {
+  constructor () {
+    super();
+    this.state = {
+      showModal: false,
+      title: '',
+      content: ''
+    };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal () {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal () {
+    this.setState({ showModal: false });
+  }
+
+  handleTitleChange(event) {
+    this.setState({title: event.target.title});
+  }
+
+  handleContentChange(event) {
+    this.setState({title: event.target.title});
+  }
+
+  handleSubmit() {
+    alert('I get called from a button!');
+  }
+
+  render () {
     return (
-      <button onClick={() => alert('I get called from a button!')}>Create a new post</button>
+      <div>
+        <button className="btn-action btn-create-post" onClick={this.handleOpenModal}>Create a new post</button>
+        <ReactModal
+          isOpen={this.state.showModal}
+          contentLabel="Create a new post"
+          ariaHideApp={false}
+        >
+          <button className="btn-action" onClick={this.handleCloseModal}>Cancel</button>
+        </ReactModal>
+      </div>
     );
   }
 }
