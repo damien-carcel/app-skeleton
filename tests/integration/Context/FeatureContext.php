81e36a58cee1d63b54ecf7ea801e4f0daf65088f
Damien Carcel <damien.carcel@akeneo.com>
2017-12-26 15:07:52 +0100

    Issue 14: Add "behat" and behat symfony extension Configure integration and acceptance tests

---
+++ tests/integration/Context/FeatureContext.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Context;
+
+use Behat\Behat\Context\Context;
+
+/**
+ * This context class contains the definitions of the steps used by the demo
+ * feature file. Learn how to get started with Behat and BDD on Behat's website.
+ *
+ * @see http://behat.org/en/latest/quick_start.html
+ */
+class FeatureContext implements Context
+{
+}
