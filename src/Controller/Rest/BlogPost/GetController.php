cd7f118435e86cecf20379fbf20e06eb303cdaee
Damien Carcel <damien.carcel@akeneo.com>
2017-12-30 17:41:34 +0100

    Issue 42: Get a blog post

---
+++ src/Controller/Rest/BlogPost/GetController.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of ApiSkeleton.
+ *
+ * Copyright (c) 2017 Damien Carcel <damien.carcel@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller\Rest\BlogPost;
+
+use App\Repository\BlogPostRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @author Damien Carcel <damien.carcel@gmail.com>
+ *
+ * @Route("/rest/blog/post/{uuid}", name="rest_blog_post_get")
+ */
+class GetController
+{
+    /** @var BlogPostRepository */
+    private $repository;
+
+    /**
+     * @param BlogPostRepository $repository
+     */
+    public function __construct(BlogPostRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param string $uuid
+     *
+     * @throws NotFoundHttpException
+     *
+     * @return Response
+     */
+    public function __invoke(string $uuid): Response
+    {
+        $post = $this->repository->find($uuid);
+
+        if (null === $post) {
+            throw new NotFoundHttpException(sprintf(
+                'There is no blog post with identifier "%s"',
+                $uuid
+            ));
+        }
+
+        $normalizedPost =  [
+            'id' => $post->id(),
+            'title' => $post->title(),
+            'content' => $post->content(),
+        ];
+
+        $response = new JsonResponse($normalizedPost);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        return $response;
+    }
+}
