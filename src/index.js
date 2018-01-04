87ec47f2d3c00bfecae69339f0e851a5983bd623
Damien Carcel <damien.carcel@gmail.com>
2018-01-04 10:06:43 +0100

    Issue 64: Fix a bug in "index.js"

--- src/index.js
+++ src/index.js
@@ -23,5 +23,5 @@ const renderedPosts = document.createElement('div');
 renderedPosts.innerHTML = listPosts(posts);
 
 const root = document.getElementById('root');
-root.addClass('container');
+root.className = 'container';
 root.appendChild(renderedPosts);
