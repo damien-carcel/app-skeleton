6afcad1b193a01b6d5b6ad0fb0dc88939dd36f31
Damien Carcel <damien.carcel@gmail.com>
2018-06-03 17:41:39 +0200

    Issue 103: Add phpspec templates

---
+++ back/.phpspec/class.tpl
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of AppSkeleton.
+ *
+ * Copyright (c) 2018 Damien Carcel <damien.carcel@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace %namespace%;
+
+/**
+ * @author Damien Carcel <damien.carcel@gmail.com>
+ */
+class %name%
+{
+}
--- back/tests/spec/Entity/BlogPostSpec.php
+++ back/tests/spec/Entity/BlogPostSpec.php
@@ -1,12 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of AppSkeleton.
+ *
+ * Copyright (c) 2018 Damien Carcel <damien.carcel@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace spec\App\Entity;
 
+use App\Entity\BlogPost;
 use PhpSpec\ObjectBehavior;
 use Ramsey\Uuid\Uuid;
 
+/**
+ * @author Damien Carcel <damien.carcel@gmail.com>
+ */
 class BlogPostSpec extends ObjectBehavior
 {
+    function it_is_a_blog_post()
+    {
+        $this->shouldBeAnInstanceOf(BlogPost::class);
+    }
+
     function it_updates_itself()
     {
         $this->update([
