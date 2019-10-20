fe4403e8b77da5304cad9e09fb36277cf2cd07cf
Damien Carcel <damien.carcel@gmail.com>
2019-10-20 10:09:22 +0200

    Issue 146: Add PHP Coupling detector

---
+++ api/.php_cd.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Akeneo\CouplingDetector\Configuration\Configuration;
+use Akeneo\CouplingDetector\Configuration\DefaultFinder;
+use Akeneo\CouplingDetector\Domain\Rule;
+use Akeneo\CouplingDetector\Domain\RuleInterface;
+
+$finder = new DefaultFinder();
+$finder->name('*.php')->in('src');
+
+
+$rules = [
+    new Rule('Carcel\User\Domain', [
+        'Carcel\User\Domain',
+        'Ramsey\Uuid\Uuid',
+        'Symfony\Component\Security\Core\User\UserInterface',
+    ], RuleInterface::TYPE_ONLY),
+    new Rule('Carcel\User\Application', [
+        'Carcel\User\Application',
+        'Carcel\User\Domain',
+        'Ramsey\Uuid\Uuid',
+    ], RuleInterface::TYPE_ONLY),
+];
+
+return new Configuration($rules, $finder);
