3a941b542d3c33f208f95b27147e3afd6c721caa
Damien Carcel <damien.carcel@akeneo.com>
2022-01-26 14:56:29 +0100

    Issue 336: Update Flex recipes

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -6,6 +6,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['acceptance' => true, 'test' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['in_memory' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
